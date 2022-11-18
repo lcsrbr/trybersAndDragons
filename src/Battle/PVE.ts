8d6761aae03c43ffc4ee28bfe1c1beefd1003557
Lucas Moura <93lucasribeiro@gmail.com>
2022-11-18 16:24:10 -0300

    ex11 - Criar a classe PVE

---
+++ src/Battle/PVE.ts
@@ -0,0 +1,24 @@
+import Fighter, { SimpleFighter } from '../Fighter';
+import Battle from './Battle';
+
+export default class PVE extends Battle {
+  constructor(
+    private _player: Fighter,
+    private _monster: (Fighter | SimpleFighter)[],
+  ) {
+    super(_player);
+  }
+
+  fight(): number {
+    if (this._player.lifePoints > 0) {
+      this._monster.forEach((creature) => {
+        this._player.attack(creature);
+        creature.attack(this._player);
+      });
+    }
+    if (this._player.lifePoints === -1) {
+      return -1;
+    } 
+    return 1;
+  }
+}
